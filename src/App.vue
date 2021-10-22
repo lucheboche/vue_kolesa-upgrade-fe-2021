cf038cc5e0cd4a56bd9c57b9486ebb30fac237b8
Vadim Mishakov <lucheboche@gmail.com>
2021-10-22 09:46:50 +0600

    VL2 Move footer in vue

--- src/App.vue
+++ src/App.vue
@@ -121,40 +121,7 @@
       </div>
     </section>
 
-    <footer class="footer">
-      <div class="footer__social">
-        <div class="footer__social-kolesa">
-          <a class="footer__social-link" href="#">© Kolesa Group</a>
-        </div>
-        <div class="footer__social-other">
-          <a class="footer__social-link" href="#">
-            <img
-              src="../src/img/Instagram.svg"
-              alt="Instagram"
-              width="24"
-              height="24"
-            />
-          </a>
-          <a class="footer__social-link" href="#">
-            <img
-              src="../src/img/YouTube.svg"
-              alt="YouTube"
-              width="24"
-              height="24"
-            />
-          </a>
-          <a class="footer__social-link" href="#">
-            <img src="../src/img/VK.svg" alt="VK" width="24" height="24" />
-          </a>
-        </div>
-      </div>
-
-      <div class="footer__text">
-        Есть идеи что улучшить?<br />Не знаешь, с кем решить проблему?
-      </div>
-
-      <button class="footer__btn">Написать</button>
-    </footer>
+    <Footer/>
   </div>
 </template>
 
@@ -162,12 +129,14 @@
 import Modal from './components/modal.vue';
 import CardItem from './components/cardItem.vue';
 import cardsMassive from './js/cardsMassive';
+import Footer from './components/footer.vue';
 
 export default {
   name: 'App',
   components: {
     Modal,
     CardItem,
+    Footer,
   },
   data() {
     return {
