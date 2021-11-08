59cb094425ed5aec8e775c34e661c63d5dd5e1fe
Vadim Mishakov <lucheboche@gmail.com>
2021-11-08 14:58:59 +0600

    VL3 move routes to .js

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import routes from './routes';
 
 Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
-    path: '/shop',
-    name: 'Shop',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "shop" */ '../views/shop/shop.vue'),
-  },
-  {
-    path: '/shop/how-get-score',
-    name: 'HowGetScore',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "how-get-score" */ '../views/shop/components/howGetScore.vue'),
-  },
-];
 
 const router = new VueRouter({
   mode: 'history',
